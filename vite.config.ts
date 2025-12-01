a919d241626c446d309ce61e16b61a93a976bec6
Jaydeep Dave <davejaydeep48@gmail.com>
2025-12-01 14:31:47 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(() => {
+  const basePath = process.env.VITE_BASE_PATH || "/";
+
+  return {
   // server: {
   //   host: "::",
   //   port: 443,
   //   allowedHosts: ["*"],
   // },
-  base: '/demo-app/',
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: basePath,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-}));
+  };
+});
